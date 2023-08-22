71e4172d6cc6440b07ab249f8025ae7307b6abee
JackAdamsJenkins <clensx@gmail.com>
2023-08-22 16:03:47 +0200

    fix filter
    Fix filter for realisation
    Add argument on onChange function
    Add type filter
    Fix date helper

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -16,7 +16,8 @@ const EventList = () => {
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
+      // Add type filter
+      : data?.events.filter((event) => event.type === type)) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
